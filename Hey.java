674a56d6fb949410f089d71d9e45c1c1ab083252
RishabhSinha <rishsinha08@gmail.com>
2019-06-18 16:15:50 +0530

    latest method overloading

--- Hey.java
+++
@@ -1,7 +0,0 @@
-public class Hey {
-    public static void main (String[] agrs)
-    {
-        System.out.println("Hey World");
-
-    }
-}
--- src/hey.java
+++ src/hey.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class hey {
+
+        public static void main (String[] agrs)
+        {
+            System.out.println("Hey World");
+
+        }
+
 }
